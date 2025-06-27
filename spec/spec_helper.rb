4b9139374ea0e7bc1bf1dbb8b8bd58d9d38722f2
Thomas Countz <thomascountz@gmail.com>
2025-06-27 13:35:30 +0200

    Use rootless Podman socket in GitHub Actions and fail if not connected properly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,11 +13,22 @@ SingleCov.setup :rspec
 
 require 'docker'
 
+# Hard-fail the build if we said we expected Podman but did not get it
+if (ENV['EXPECT_PODMAN'] == 'true') && !Docker.podman?
+  warn <<~MSG
+    EXPECT_PODMAN=true was set, but Docker.podman? returned false.
+    Connected engine information:
+    #{Docker.version}
+    Make sure the Podman API socket is running and DOCKER_HOST points to it.
+  MSG
+  exit 1
+end
+
 ENV['DOCKER_API_USER']  ||= 'debbie_docker'
 ENV['DOCKER_API_PASS']  ||= '*************'
 ENV['DOCKER_API_EMAIL'] ||= 'debbie_docker@example.com'
 
-RSpec.shared_context "local paths" do
+RSpec.shared_context 'local paths' do
   def project_dir
     File.expand_path(File.join(File.dirname(__FILE__), '..'))
   end
@@ -25,7 +36,7 @@ end
 
 module SpecHelpers
   def skip_without_auth
-    skip "Disabled because of missing auth" if ENV['DOCKER_API_USER'] == 'debbie_docker'
+    skip 'Disabled because of missing auth' if ENV['DOCKER_API_USER'] == 'debbie_docker'
   end
 end
 
